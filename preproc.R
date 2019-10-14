be7656a0c7eb1a90a62f0927ed61cd3888f28562
Andrew Couch <andrew.couch.ia@gmail.com>
2019-10-14 15:52:22 -0500

    Added summary chart

--- preproc.R
+++ preproc.R
@@ -3,12 +3,17 @@ library(tidyverse)
 library(GGally)
 
 harrypotter <- readRDS("harrypotter.rds")
+
 upModel <- readRDS("upModel.rds")
 
-
-
 PCAExtra <- predict(preProcess(harrypotter, method = c("BoxCox","corr", "pca")), harrypotter)
+
 CorrTrans <- predict(preProcess(harrypotter, method = c("corr")), harrypotter) 
+
 CorrPCA <- predict(preProcess(harrypotter, method = c("corr", "pca")), harrypotter)
-nzv <- predict(preProcess(harrypotter, method = c("nzv")), harrypotter)
+
 spatial <- predict(preProcess(harrypotter, method = c("spatialSign")), harrypotter)
+
+BigCombo <- predict(preProcess(harrypotter, method = c("center","scale","YeoJohnson","nzv")))
+
+CorrNonZeroVariance <- predict(preProcess(harrypotter, method = c("center","scale","YeoJohnson", "corr", "nzv")), harrypotter)
